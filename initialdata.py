ebab7a59f4b44b224066815ab8d6d92f06cb3619
mvaysiberg <mvays11@gmail.com>
2020-07-09 10:48:10 -0400

    number of rounds data

--- initialdata.py
+++ initialdata.py
@@ -16,3 +16,18 @@ print(round_one.describe())
 #75%        58.000000
 #max        85.000000
 #Name: Deadwood, dtype: float64
+
+num_rounds = data.groupby("Game")["Round"].max()
+print(num_rounds.describe())
+
+#OUTPUT
+
+#count    5163.000000
+#mean        6.748983
+#std         2.700133
+#min         1.000000
+#25%         5.000000
+#50%         7.000000
+#75%         8.000000
+#max        16.000000
+#Name: Round, dtype: float64
