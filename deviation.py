b26bae1c0a24d847233bb3947343f0047f42b5e5
mvaysiberg <mvays11@gmail.com>
2020-07-26 10:42:22 -0400

    updated  deviation data

--- deviation.py
+++ deviation.py
@@ -6,25 +6,25 @@ non_zero_data = data.loc[data["Max value deviation"] != 0.0]
 print(data.describe())
 #OUTPUT
 #Max value deviation
-#count         73404.000000
-#mean             11.559865
-#std              16.887943
+#count          7477.000000
+#mean            112.627792
+#std              90.479094
 #min               0.000000
-#25%               0.000000
-#50%               0.000000
-#75%              19.333333
-#max             109.333333
+#25%              38.000000
+#50%              94.000000
+#75%             169.333333
+#max             554.666667
 
 print()
 print(non_zero_data.describe())
 
 #OUTPUT
 #Max value deviation
-#count         36229.000000
-#mean             23.421578
-#std              17.321453
+#count          7144.000000
+#mean            117.877660
+#std              89.158191
 #min               0.333333
-#25%              10.000000
-#50%              20.000000
-#75%              33.333333
-#max             109.333333
+#25%              44.000000
+#50%             100.000000
+#75%             174.416667
+#max             554.666667
